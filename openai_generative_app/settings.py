abb377960fcd59128990e5354369035397eae95f
scott <scott@localhost>
2024-03-30 13:11:07 -0700

    updating allowed_hosts for Docker

--- openai_generative_app/settings.py
+++ openai_generative_app/settings.py
@@ -31,10 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: update this when you have the production host
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
